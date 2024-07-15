a1cb6a67ac1558cc9aef7570b8d54643b8d74484
Harsh Dana <harshdana1@gmail.com>
2024-07-15 20:03:37 +0530

    more fixes

--- src/pages/NetworkStatus.tsx
+++ src/pages/NetworkStatus.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { toast } from "sonner"; // Assuming you're using sonner for notifications
 
 const NetworkStatus = () => {
